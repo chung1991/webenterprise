7a29831b32e90c8e1a59d28b70a58c35a646b13a
chung1991 <chung1991@Chung>
2016-02-06 20:54:35 +0700

    Add base project

---
+++ CMR/CMR/Layers/DataAccessLayer/SalesERPDAL.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMR.Models;
+
+namespace CMR.Layers.DataAccessLayer
+{
+    public class SalesERPDAL : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("TblEmployee");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CMR/CMR/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CMR.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Enter First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(5, ErrorMessage = "Last Name length should not be greater than 5")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Enter Salary")]
+        public int Salary { get; set; }
+    }
+}
