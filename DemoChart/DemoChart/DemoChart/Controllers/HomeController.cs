213f0078058b56b2008307fde763c32174328d6a
thanh.pham <thanhpdgc00489@fpt.edu.vn>
2016-02-24 15:30:37 +0700

    Demo chart and generating PDF file

---
+++ DemoChart/DemoChart/DemoChart/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc;
+using DemoChart.Models;
+using DemoChart.Utilities;
+
+namespace DemoChart.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var chartData = BrowserShareRepository.GetBrowserShares();
+            return View(chartData);
+        }
+
+        [HttpGet]
+        public FileResult GetChart()
+        {
+            var chartData = BrowserShareRepository.GetBrowserShares();
+            return File(chartData.ChartImageStream().GetBuffer()
+                , @"image/png", "BrowserShareChart.png");
+        }
+
+        [HttpGet]
+        public FileResult GetPdf()
+        {
+            var chartData = BrowserShareRepository.GetBrowserShares();
+            var chartStream = chartData.ChartImageStream();
+
+            return File(PdfUtility.GetSimplePdf(chartStream).GetBuffer()
+                , @"application/pdf", "BrowserShareChart.pdf");
+        }
+    }
+}
---
+++ DemoChart/DemoChart/DemoChart/Utilities/PdfUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace DemoChart.Utilities
+{
+    public class PdfUtility
+    {
+        // Create a simple Pdf document and add an image to it.
+        public static MemoryStream GetSimplePdf(MemoryStream chartImage)
+        {
+            const int documentMargin = 10;
+
+            var pdfStream = new MemoryStream();
+            var pdfDocument = new Document(PageSize.LETTER);
+            pdfDocument.SetMargins(documentMargin, documentMargin,
+                                   documentMargin, documentMargin);
+            PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDocument, pdfStream);
+
+            Image image = Image.GetInstance(chartImage.GetBuffer());
+            image.SetAbsolutePosition(documentMargin
+                , pdfDocument.PageSize.Height - documentMargin - image.ScaledHeight);
+
+            pdfDocument.Open();
+            pdfDocument.Add(image);
+            pdfDocument.Close();
+            pdfWriter.Flush();
+
+            return pdfStream;
+        }
+    }
+}
