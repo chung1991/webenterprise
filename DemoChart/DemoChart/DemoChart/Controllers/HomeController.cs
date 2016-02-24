1d3c45c186c57f0fa22e27509c54fb817483c1fd
thanh.pham <thanhpdgc00489@fpt.edu.vn>
2016-02-24 17:23:54 +0700

    Add more style chart

--- DemoChart/DemoChart/DemoChart/Controllers/HomeController.cs
+++ DemoChart/DemoChart/DemoChart/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc;
 using DemoChart.Models;
 using DemoChart.Utilities;
+using System.Configuration;
+using System.Web.Routing;
+using System.Web.SessionState;
+using System.Web.Helpers;
 
 namespace DemoChart.Controllers
 {
@@ -35,6 +38,39 @@ namespace DemoChart.Controllers
 
             return File(PdfUtility.GetSimplePdf(chartStream).GetBuffer()
                 , @"application/pdf", "BrowserShareChart.pdf");
+        }
+
+        public ActionResult CreateBar()
+        {
+            //Create bar chart
+            var chart = new Chart(width:300,height:200)
+            .AddSeries(     chartType: "bar",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
+        }
+
+        public ActionResult CreatePie()
+        {
+            //Create bar chart
+            var chart = new Chart(width: 300, height: 200)
+            .AddSeries(chartType: "pie",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
+        }
+
+        public ActionResult CreateLine()
+        {
+            //Create bar chart
+            var chart = new Chart(width: 600, height: 200)
+            .AddSeries(chartType: "line",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
         }
     }
 }
