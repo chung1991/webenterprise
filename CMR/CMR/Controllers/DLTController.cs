9c376dd4c911538ae2ad73ecb2252b09122bb1bf
Nguyen Tien Dung <dungnt19394@gmail.com>
2016-03-26 22:08:27 +0700

    no message

---
+++ CMR/CMR/Controllers/DLTController.cs
@@ -0,0 +1,18 @@
+﻿using CMR.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMR.Controllers
+{
+    [CustomAuthorize(Roles = "DLT")]
+    public class DLTController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
